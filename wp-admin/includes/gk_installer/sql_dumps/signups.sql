9d3dd0c66e1896b8feebbba5b7b99e3337c15079
prdanielmota <pr.danielmota@gmail.com>
2015-06-28 22:40:51 -0300

    Upload de Arquivos

---
+++ wp-admin/includes/gk_installer/sql_dumps/signups.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `{$table_prefix}signups` (
+  `signup_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `domain` varchar(200) NOT NULL DEFAULT '',
+  `path` varchar(100) NOT NULL DEFAULT '',
+  `title` longtext NOT NULL,
+  `user_login` varchar(60) NOT NULL DEFAULT '',
+  `user_email` varchar(100) NOT NULL DEFAULT '',
+  `registered` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `activated` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `activation_key` varchar(50) NOT NULL DEFAULT '',
+  `meta` longtext,
+  PRIMARY KEY (`signup_id`),
+  KEY `activation_key` (`activation_key`),
+  KEY `user_email` (`user_email`),
+  KEY `user_login_email` (`user_login`,`user_email`),
+  KEY `domain_path` (`domain`,`path`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
